17ba4f61d7769a125c664144c411813fd469ab4f
Nawaz Dhandala <nawazdhandala@outlook.com>
2016-02-26 10:31:04 -0300

    Reset Password Email working

--- services/mail.js
+++ services/mail.js
@@ -1,6 +1,7 @@
-var nodemailer = require('nodemailer');
 var smtpConfig = null;
 var defaultTransporter = null;
+var mandrill = require('mandrill-api/mandrill');
+
 
 module.exports = function(){
          
@@ -11,29 +12,15 @@ module.exports = function(){
         try{
             console.log("In mail service");
             smtpConfig = require('../config/smtp.json');
-            var transporterConnectionString = "";
-            if(smtpConfig.smtps){
-                transporterConnectionString = "smtps://"
-            }else{
-                transporterConnectionString = "smtp://"
-            }
-            transporterConnectionString+=smtpConfig.username.replace("@","%40")+":"+smtpConfig.password+"@"+smtpConfig.server;
-            defaultTransporter = nodemailer.createTransport(transporterConnectionString);
-            console.log("Mail Transporter Status : OK");
+            var mandrill_client = new mandrill.Mandrill(smtpConfig.mandrill_api_key);
         }catch(e){
             //probably file not found or is in incorrect format.
             console.log("Mail services disabled because SMTP Config not found or is invalid. Please add correct smtp.json in config to enable mail services."); 
             smtpConfig = null;
         }    
-                    
-        console.log("Sending an email.");
-        console.log("App ID : "+appId);
-        console.log("To "+to);
-        console.log("Subject : "+subject);
-        console.log("Text "+text);
         
         var deferred = q.defer();
-   
+        
         // create reusable transporter object using the default SMTP transport
         if(!smtpConfig){
             return;
@@ -43,27 +30,28 @@ module.exports = function(){
             from = smtpConfig.from;
         }
         
-        // setup e-mail data with unicode symbols
-        var mailOptions = {
-            from: from, // sender address
-            to: to, // list of receivers
-            subject: subject, // Subject line
-            text: text, // plaintext body
-            html: html // html body
+        var fromName  = null;
+        
+        if(!fromName)
+            fromName = smtpConfig.from_name;
+        
+        var message = {
+            "html": html,
+            "text": text,
+            "subject": subject,
+            "from_email": from,
+            "from_name": fromName,
+            "to": [{
+                    "email": to
+                }]
         };
         
-        console.log("Sending...");
+        var async = false;
         
-        // send mail with defined transport object
-        defaultTransporter.sendMail(mailOptions, function(error, info){
-            if(error){
-                console.log("Mail Error.");
-                console.log(JSON.stringify(error));    
-                deferred.reject(error);
-            }else{
-                console.log('Message sent : ' + info.response);
-                deferred.resolve();
-            }
+        mandrill_client.messages.send({"message": message, "async": async, "ip_pool": null, "send_at": null}, function(result) {
+            deferred.resolve();
+        }, function(e) {
+            deferred.reject(e);
         });
         
         return deferred.promise;
