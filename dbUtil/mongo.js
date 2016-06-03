a87ad59d81c5a1d6046d66863c8feba2ac4b56f4
rtbathula <battu.network@gmail.com>
2016-06-03 15:07:41 +0530

    implimented search in mongo

--- dbUtil/mongo.js
+++ dbUtil/mongo.js
@@ -259,7 +259,7 @@ module.exports = function () {
                 
                 var collection =  global.mongoClient.db(appId).collection(_self.collection.getId(appId, collectionName));
 
-                //Freshly Drop and Create 
+                //Drop and Create freshly 
                 collection.dropIndex(indexString,function(err,result){
                     if(err && err.message && err.message!= 'ns not found') {
                         
