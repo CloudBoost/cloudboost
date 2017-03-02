5163a16e46c2fa2800bebbce7c9cc1c6440a152b
kashishchakarvarti <kashpsyranger@gmail.com>
2017-03-02 15:21:32 +0530

    api/tables/CloudObjects.js

--- api/tables/CloudObjects.js
+++ api/tables/CloudObjects.js
@@ -11,7 +11,7 @@ var customHelper = require('../../helpers/custom.js');
 module.exports = function() {
 
 
-    global.app.put('/data/:appId/:tableName', function (req, res, next) { //save a new document into <tableName> of app
+    global.app.put('/data/:appId/:tableName', function (req, res) { //save a new document into <tableName> of app
         if(req.body && req.body.method=="DELETE"){
             /******************DELETE API*********************/
             _deleteApi(req, res);
@@ -22,7 +22,6 @@ module.exports = function() {
             var appId = req.params.appId;
             var document = req.body.document;
             var collectionName = req.params.tableName;
-            var userId = req.session.userId || null;
             var appKey = req.body.key || req.params.key;
             var sdk = req.body.sdk || "REST";
             
@@ -58,11 +57,10 @@ module.exports = function() {
    
 	global.app.delete('/data/:appId/:tableName', _deleteApi);
 
-    function _deleteApi(req, res, next) { //delete a document matching the <objectId>
+    function _deleteApi(req, res) { //delete a document matching the <objectId>
         console.log("DELETE API");
         var appId = req.params.appId;
         var collectionName = req.params.tableName;
-        var userId = req.session.userId || null;
         var document = req.body.document;
         var appKey = req.body.key || req.param('key');
         var sdk = req.body.sdk || "REST";
@@ -81,7 +79,7 @@ module.exports = function() {
 
 };
 
-function _getData(req, res, next) { //get document(s) object based on query and various parameters
+function _getData(req, res) { //get document(s) object based on query and various parameters
     console.log("FIND API");
     var appId = req.params.appId;
     var collectionName = req.params.tableName;
@@ -90,12 +88,11 @@ function _getData(req, res, next) { //get document(s) object based on query and 
     var sort = req.body.sort;
     var limit = req.body.limit;
     var skip = req.body.skip;
-    var userId = req.session.userId || null;
     var appKey = req.body.key || req.param('key');
     var sdk = req.body.sdk || "REST";
     
     global.appService.isMasterKey(appId, appKey).then(function (isMasterKey) {
-        return global.customService.find(appId, collectionName, query, select, sort, limit, skip, customHelper.getAccessList(req), isMasterKey)
+        return global.customService.find(appId, collectionName, query, select, sort, limit, skip, customHelper.getAccessList(req), isMasterKey);
     }).then(function (results) {
         res.json(results);
     }, function (error) {
@@ -105,14 +102,13 @@ function _getData(req, res, next) { //get document(s) object based on query and 
     global.apiTracker.log(appId,"Object / Find", req.url,sdk);
 }
 
-function _count(req, res, next) { //get document(s) object based on query and various parameters
+function _count(req, res) { //get document(s) object based on query and various parameters
     console.log("COUNT API");
     var appId = req.params.appId;
     var collectionName = req.params.tableName;
     var query = req.body.query;
     var limit = req.body.limit;
     var skip = req.body.skip;
-    var userId = req.session.userId || null;
     var appKey = req.body.key || req.param('key');
     var sdk = req.body.sdk || "REST";
     
@@ -137,7 +133,6 @@ function _distinct(req, res, next) { //get document(s) object based on query and
     var sort = req.body.sort;
     var limit = req.body.limit;
     var skip = req.body.skip;
-    var userId = req.session.userId || null;
     var appKey = req.body.key || req.param('key');
     var sdk = req.body.sdk || "REST";
     
@@ -152,7 +147,7 @@ function _distinct(req, res, next) { //get document(s) object based on query and
     global.apiTracker.log(appId,"Object / Distinct", req.url,sdk);
 }
 
-function _findOne(req, res, next) { //get a single document matching the search query
+function _findOne(req, res) { //get a single document matching the search query
     console.log("FIND ONE API");
     var appId = req.params.appId;
     var collectionName = req.params.tableName;
@@ -160,7 +155,6 @@ function _findOne(req, res, next) { //get a single document matching the search 
     var select = req.body.select;
     var sort = req.body.sort;
     var skip = req.body.skip;
-    var userId = req.session.userId || null;
     var appKey = req.body.key || req.param('key');
     var sdk = req.body.sdk || "REST";
     
