b49fc25292c5ddf8346ff8282a322595d82bd003
kashishchakarvarti <kashpsyranger@gmail.com>
2017-05-10 15:45:39 +0530

    export changes

--- api/app/App.js
+++ api/app/App.js
@@ -296,7 +296,7 @@ module.exports = function() {
             global.appService.isMasterKey(appId,appKey).then(function(isMasterKey) {         
                 global.appService.exportTable(appId,tableName,exportType.toLowerCase(),isMasterKey,accessList).then(function(data) {
                     if(exportType.toLowerCase()==='json'){
-                        res.status(200).json({data:data});
+                        res.status(200).json({data});
                     }else{ res.status(200).send(data);}
                 }, function(err) {
                         console.log("Error : Exporting Table.");
