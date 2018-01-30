6959c98341e391e7ea7345bb5f371ee97cb8a1ff
Ravi Exploit <rravithejareddy@gmail.com>
2018-01-31 02:32:05 +0530

    changes in grunt

--- test/email/CloudEmail.js
+++ test/email/CloudEmail.js
@@ -1,13 +1,3 @@
-var window = window || null;
-var request = require('request');
-var SECURE_KEY = "37ac59f0-7eab-49b8-8bc9-e121b35ec2b6";
-var URL = "http://localhost:4730";
-var equal = require('deep-equal');
-var util = require('./Util.js');
-var testAppId = null;
-var testKey = null;
-var testMasterKey = null;
-
 describe("CloudBoost Email API", function () {
 
    it('should send emails successfully', done => {
--- test/users/CloudUsers.js
+++ test/users/CloudUsers.js
@@ -158,35 +158,4 @@ describe("CloudBoost Users API", function () {
     }
   });
 
-
-  describe("Delete App", function () {
-    it("should delete the app and teardown", function (done) {
-      this.timeout(100000);
-      var appId = util.makeString();
-      var url = URL + '/app/' + testAppId;
-      var params = {};
-      params.secureKey = testKey;
-      params.method = "DELETE";
-
-      if (!window) {
-        //Lets configure and request
-        request({
-          url: url, //URL to hit
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          json: params //Set the body as a string
-        }, function (error, response, body) {
-          if (error) {
-            done(error);
-          } else {
-            done();
-          }
-        });
-      }
-
-    });
-  });
-
 });
