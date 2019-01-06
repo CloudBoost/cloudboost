1a2196b3c12e18bbdde394daeacde8fb73ff432f
Sauban <saubanlawal@gmail.com>
2019-01-06 17:46:58 +0100

    Modified api/server/Server.js to es6 async-await

--- api/server/Server.js
+++ api/server/Server.js
@@ -12,33 +12,46 @@ const config = require('../../config/config');
 const serverService = require('../../services/server');
 const keyService = require('../../database-connect/keyService');
 
-module.exports = function (app) {
+module.exports = (app) => {
   // Description : Used to change server URL form localhost to any DNS.
   // Params : secureKey : Used to validate the request.
   //         url : New Server URL.
   // Returns : 200 - success
   //          400 - Invalid URL, 401 - Unauthoroized, 500 - Internal Server Error.
-  app.post('/server/url', (req, res) => {
+  app.post('/server/url', async (req, res) => {
     if (!util.isUrlValid(req.body.url)) {
       return res.status(400).send('Invalid URL');
     }
 
     if (config.secureKey === req.body.secureKey) {
-      keyService.changeUrl(req.body.url).then((url) => {
-        res.status(200).send({ status: 'success', message: `Cluster URL Updated to ${url}` });
-      }, (err) => {
-        winston.error({
-          error: err,
+      try {
+        const url = await keyService.changeUrl(req.body.url);
+        return res.status(200).json({
+          status: 'success',
+          message: `Cluster URL Updated to ${url}`,
         });
-        res.status(500).send('Error, Cannot change the cluster URL at this time.');
-      });
+      } catch (error) {
+        winston.error({
+          error,
+        });
+        return res.status(500).send('Error, Cannot change the cluster URL at this time.');
+      }
     } else {
-      res.status(401).send('Unauthorized');
+      return res.status(401).send('Unauthorized');
     }
   });
 
 
-  app.get('/status', (req, res) => {
-    serverService.getDBStatuses().then(() => res.status(200).json({ status: 200, message: 'Service Status : OK' }), error => res.status(500).send(error));
+  app.get('/status', async (req, res) => {
+    try {
+      await serverService.getDBStatuses();
+      return res.status(200).json({
+        status: 200,
+        message: 'Service status : OK',
+      });
+    } catch (error) {
+      winston.error({ error });
+      return res.status(500).send(error);
+    }
   });
 };
