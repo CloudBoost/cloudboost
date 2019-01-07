929c63dbc323be2ac8447a9b23b6876987da5be1
Sauban <saubanlawal@gmail.com>
2019-01-07 16:51:04 +0100

    Modified api/tables/CloudObjects.js to es6 async-await

--- api/tables/CloudObjects.js
+++ api/tables/CloudObjects.js
@@ -3,7 +3,7 @@
 #     (c) 2014 HackerBay, Inc.
 #     CloudBoost may be freely distributed under the Apache 2 License
 */
-
+const winston = require('winston');
 const customHelper = require('../../helpers/custom.js');
 const integrationService = require('../../services/integrationService');
 
@@ -11,151 +11,209 @@ const apiTracker = require('../../database-connect/apiTracker');
 const customService = require('../../services/cloudObjects');
 const appService = require('../../services/app');
 
-module.exports = function (app) {
-  app.put('/data/:appId/:tableName', (req, res) => { // save a new document into <tableName> of app
-    if (req.body && req.body.method == 'DELETE') {
+const deleteApi = async (req, res) => { // delete a document matching the <objectId>
+  const { appId, tableName: collectionName } = req.params;
+  const { document, sdk = 'REST' } = req.body;
+  const appKey = req.body.key || req.param('key');
+
+  apiTracker.log(appId, 'Object / Delete', req.url, sdk);
+
+  try {
+    const isMasterKey = await appService.isMasterKey(appId, appKey);
+    const result = await customService.delete(
+      appId,
+      collectionName,
+      document,
+      customHelper.getAccessList(req),
+      isMasterKey,
+    );
+    integrationService.integrationNotification(appId, document, collectionName, 'Delete');
+    res.json(result);
+  } catch (error) {
+    winston.error({ error });
+    res.status(400).send(error);
+  }
+};
+
+// get document(s) object based on query and various parameters
+const getData = async (req, res) => {
+  const { appId, tableName: collectionName } = req.params;
+  const {
+    query,
+    select,
+    sort,
+    limit,
+    skip,
+    sdk = 'REST',
+  } = req.body;
+  const appKey = req.body.key || req.param('key');
+
+  apiTracker.log(appId, 'Object / Find', req.url, sdk);
+
+  try {
+    const isMasterKey = await appService.isMasterKey(appId, appKey);
+    const results = await customService.find(
+      appId,
+      collectionName,
+      query,
+      select,
+      sort,
+      limit,
+      skip,
+      customHelper.getAccessList(req),
+      isMasterKey,
+    );
+    res.json(results);
+  } catch (error) {
+    winston.error({ error });
+    res.status(400).send(error);
+  }
+};
+
+const count = async (req, res) => { // get document(s) object based on query and various parameters
+  const { appId, tableName: collectionName } = req.params;
+  const {
+    query,
+    limit,
+    skip,
+    sdk = 'REST',
+  } = req.body;
+  const appKey = req.body.key || req.param('key');
+
+  apiTracker.log(appId, 'Object / Count', req.url, sdk);
+
+  try {
+    const isMasterKey = await appService.isMasterKey(appId, appKey);
+    const result = await customService.count(
+      appId,
+      collectionName,
+      query,
+      limit,
+      skip,
+      customHelper.getAccessList(req),
+      isMasterKey,
+    );
+    res.json(result);
+  } catch (error) {
+    winston.error({ error });
+    res.status(400).send(error);
+  }
+};
+
+// get document(s) object based on query and various parameters
+const distinct = async (req, res) => {
+  const { appId, tableName: collectionName } = req.params;
+  const {
+    onKey,
+    query,
+    limit,
+    skip,
+    sdk = 'REST',
+    select,
+    sort,
+  } = req.body;
+  const appKey = req.body.key || req.param('key');
+
+  apiTracker.log(appId, 'Object / Distinct', req.url, sdk);
+
+  try {
+    const isMasterKey = await appService.isMasterKey(appId, appKey);
+    const results = await customService.distinct(
+      appId,
+      collectionName,
+      onKey,
+      query,
+      select,
+      sort,
+      limit,
+      skip,
+      customHelper.getAccessList(req),
+      isMasterKey,
+    );
+    res.json(results);
+  } catch (error) {
+    winston.error({ error });
+    res.status(400).send(error);
+  }
+};
+
+const findOne = async (req, res) => { // get a single document matching the search query
+  const { appId, tableName: collectionName } = req.params;
+  const {
+    query,
+    select,
+    sort,
+    skip,
+    sdk = 'REST',
+  } = req.body;
+  const appKey = req.body.key || req.param('key');
+
+  apiTracker.log(appId, 'Object / FindOne', req.url, sdk);
+
+  try {
+    const isMasterKey = await appService.isMasterKey(appId, appKey);
+    const results = await customService.findOne(
+      appId,
+      collectionName,
+      query,
+      select,
+      sort,
+      skip,
+      customHelper.getAccessList(req),
+      isMasterKey,
+    );
+    res.json(results);
+  } catch (error) {
+    winston.error({ error });
+    res.status(400).send(error);
+  }
+};
+
+module.exports = (app) => {
+  app.put('/data/:appId/:tableName', async (req, res) => { // save a new document into <tableName> of app
+    if (req.body && req.body.method === 'DELETE') {
       /** ****************DELETE API******************** */
-      _deleteApi(req, res);
+      deleteApi(req, res);
       /** ****************DELETE API******************** */
     } else {
       /** ****************SAVE API******************** */
 
-      const appId = req.params.appId;
-      const document = req.body.document;
-      const collectionName = req.params.tableName;
+      const { appId, document, tableName: collectionName } = req.params;
       const appKey = req.body.key || req.params.key;
       const sdk = req.body.sdk || 'REST';
-      let table_event = '';
-
-      if (document._id) {
-        table_event = 'Update';
-      } else {
-        table_event = 'Create';
-      }
-      appService.isMasterKey(appId, appKey).then((isMasterKey) => {
-        appService.getApp(appId).then((application) => {
-          customService.save(appId, collectionName, document, customHelper.getAccessList(req), isMasterKey, null, application.keys.encryption_key).then((result) => {
-            integrationService.integrationNotification(appId, document, collectionName, table_event);
-            res.status(200).send(result);
-          }, (error) => {
-            res.status(400).send(error);
-          });
-        }, () => {
-          res.status(400).send('App not found.');
-        });
-      });
-
-      apiTracker.log(appId, 'Object / Save', req.url, sdk);
+      const tableEvent = document._id // eslint-disable-line no-underscore-dangle
+        ? 'Update' : 'Create';
       /** ****************SAVE API******************** */
+      apiTracker.log(appId, 'Object / Save', req.url, sdk);
+      try {
+        const isMasterKey = await appService.isMasterKey(appId, appKey);
+        const application = await appService.getApp(appId);
+        const result = await customService.save(
+          appId,
+          collectionName,
+          document,
+          customHelper.getAccessList(req),
+          isMasterKey, null, application.keys.encryption_key,
+        );
+        integrationService.integrationNotification(appId, document, collectionName, tableEvent);
+        res.status(200).send(result);
+      } catch (error) {
+        res.status(400).send(error);
+      }
     }
   });
 
-  app.get('/data/:appId/:tableName/find', _getData);
-  app.post('/data/:appId/:tableName/find', _getData);
+  app.get('/data/:appId/:tableName/find', getData);
+  app.post('/data/:appId/:tableName/find', getData);
 
-  app.get('/data/:appId/:tableName/count', _count);
-  app.post('/data/:appId/:tableName/count', _count);
+  app.get('/data/:appId/:tableName/count', count);
+  app.post('/data/:appId/:tableName/count', count);
 
-  app.get('/data/:appId/:tableName/distinct', _distinct);
-  app.post('/data/:appId/:tableName/distinct', _distinct);
+  app.get('/data/:appId/:tableName/distinct', distinct);
+  app.post('/data/:appId/:tableName/distinct', distinct);
 
-  app.get('/data/:appId/:tableName/findOne', _findOne);
-  app.post('/data/:appId/:tableName/findOne', _findOne);
+  app.get('/data/:appId/:tableName/findOne', findOne);
+  app.post('/data/:appId/:tableName/findOne', findOne);
 
-  app.delete('/data/:appId/:tableName', _deleteApi);
-
-  function _deleteApi(req, res) { // delete a document matching the <objectId>
-    const appId = req.params.appId;
-    const collectionName = req.params.tableName;
-    const document = req.body.document;
-    const appKey = req.body.key || req.param('key');
-    const sdk = req.body.sdk || 'REST';
-
-    appService.isMasterKey(appId, appKey).then(isMasterKey => customService.delete(appId, collectionName, document, customHelper.getAccessList(req), isMasterKey)).then((result) => {
-      integrationService.integrationNotification(appId, document, collectionName, 'Delete');
-      res.json(result);
-    }, (error) => {
-      res.status(400).send(error);
-    });
-
-    apiTracker.log(appId, 'Object / Delete', req.url, sdk);
-  }
+  app.delete('/data/:appId/:tableName', deleteApi);
 };
-
-function _getData(req, res) { // get document(s) object based on query and various parameters
-  const appId = req.params.appId;
-  const collectionName = req.params.tableName;
-  const query = req.body.query;
-  const select = req.body.select;
-  const sort = req.body.sort;
-  const limit = req.body.limit;
-  const skip = req.body.skip;
-  const appKey = req.body.key || req.param('key');
-  const sdk = req.body.sdk || 'REST';
-
-  appService.isMasterKey(appId, appKey).then(isMasterKey => customService.find(appId, collectionName, query, select, sort, limit, skip, customHelper.getAccessList(req), isMasterKey)).then((results) => {
-    res.json(results);
-  }, (error) => {
-    res.status(400).send(error);
-  });
-
-  apiTracker.log(appId, 'Object / Find', req.url, sdk);
-}
-
-function _count(req, res) { // get document(s) object based on query and various parameters
-  const appId = req.params.appId;
-  const collectionName = req.params.tableName;
-  const query = req.body.query;
-  const limit = req.body.limit;
-  const skip = req.body.skip;
-  const appKey = req.body.key || req.param('key');
-  const sdk = req.body.sdk || 'REST';
-
-  appService.isMasterKey(appId, appKey).then(isMasterKey => customService.count(appId, collectionName, query, limit, skip, customHelper.getAccessList(req), isMasterKey)).then((result) => {
-    res.json(result);
-  }, (error) => {
-    res.status(400).send(error);
-  });
-
-  apiTracker.log(appId, 'Object / Count', req.url, sdk);
-}
-
-function _distinct(req, res) { // get document(s) object based on query and various parameters
-  const appId = req.params.appId;
-  const collectionName = req.params.tableName;
-  const onKey = req.body.onKey;
-  const query = req.body.query;
-  const select = req.body.select;
-  const sort = req.body.sort;
-  const limit = req.body.limit;
-  const skip = req.body.skip;
-  const appKey = req.body.key || req.param('key');
-  const sdk = req.body.sdk || 'REST';
-
-  appService.isMasterKey(appId, appKey).then(isMasterKey => customService.distinct(appId, collectionName, onKey, query, select, sort, limit, skip, customHelper.getAccessList(req), isMasterKey)).then((results) => {
-    res.json(results);
-  }, (error) => {
-    res.status(400).send(error);
-  });
-
-  apiTracker.log(appId, 'Object / Distinct', req.url, sdk);
-}
-
-function _findOne(req, res) { // get a single document matching the search query
-  const appId = req.params.appId;
-  const collectionName = req.params.tableName;
-  const query = req.body.query;
-  const select = req.body.select;
-  const sort = req.body.sort;
-  const skip = req.body.skip;
-  const appKey = req.body.key || req.param('key');
-  const sdk = req.body.sdk || 'REST';
-
-  appService.isMasterKey(appId, appKey).then(isMasterKey => customService.findOne(appId, collectionName, query, select, sort, skip, customHelper.getAccessList(req), isMasterKey)).then((result) => {
-    res.json(result);
-  }, (error) => {
-    res.status(400).send(error);
-  });
-
-  apiTracker.log(appId, 'Object / FindOne', req.url, sdk);
-}
