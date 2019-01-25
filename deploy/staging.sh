99b5c047c334e6e4d8dd54c6fdc6bc74070975d3
Sauban <saubanlawal@gmail.com>
2019-01-25 19:39:12 +0100

    Debugging script

--- deploy/staging.sh
+++ deploy/staging.sh
@@ -3,18 +3,16 @@
 set -e
 
 echo "$DOCKERPASSWORD" | docker login -u "$DOCKERUSERNAME" --password-stdin;
-# docker push cloudboost/cloudboost:master-2.0.$TRAVIS_BUILD_NUMBER;
+echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i > ${HOME}/gcloud-service-key.json;
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json;
 
-echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+gcloud --quiet config set project $PROJECT_NAME_PRD;
+gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD;
+gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE};
+gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD;
 
-gcloud --quiet config set project $PROJECT_NAME_PRD
-gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
-gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
-gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
-
-kubectl config view
-kubectl config current-context
+kubectl config view;
+kubectl config current-context;
 
 kubectl set image deployment cloudboost-api cloudboost-api=cloudboost/cloudboost:master-2.0.860;
 kubectl rollout status deployment cloudboost-api;
