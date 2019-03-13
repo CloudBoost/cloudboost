3c2eeb6a9f12bab11c72140acb08c33a3fbdc105
Otse Obande <otseobande@gmail.com>
2019-03-13 13:12:29 +0100

    chore(config): remove elastic search config

--- config/cloudboost.js
+++ config/cloudboost.js
@@ -3,10 +3,6 @@ module.exports = {
     host: process.env.MONGO_HOST || 'localhost',
     port: '27017',
   }],
-  elasticsearch: [{
-    host: process.env.ELASTIC_SEARCH_HOST || 'localhost',
-    port: '9200',
-  }],
   redis: [{
     host: process.env.REDIS_HOST || 'localhost',
     port: 6379,
